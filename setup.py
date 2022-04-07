af9e55c80b31a4675ed767622dca7761ac527dc6
DarkCat09 <a.chechkenev@yandex.ru>
2022-04-07 11:32:13 +0400

    Some changes in setup

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ import setuptools
 with open('README.md', 'rt') as readme:
 	long_description = readme.read()
 
-with open('requirements.txt', 'rt') as f:
-	requires = f.readlines()
-	for i, r in enumerate(requires):
-		requires[i] = r.strip('\r\n')
-
 setuptools.setup(
 	name='python-aternos',
 	version='0.6',
@@ -18,8 +13,7 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url='https://github.com/DarkCat09/python-aternos',
 	project_urls={
-		'Bug Tracker': 'https://github.com/DarkCat09/python-aternos/issues',
-		'Documentation': 'https://github.com/DarkCat09/python-aternos/wiki/Client-(entry-point)',
+		'Bug Tracker': 'https://github.com/DarkCat09/python-aternos/issues'
 	},
 	classifiers=[
 		'Development Status :: 4 - Beta',
@@ -27,7 +21,14 @@ setuptools.setup(
 		'License :: OSI Approved :: Apache Software License',
 		'Operating System :: OS Independent'
 	],
-	install_requires=requires,
+	install_requires=[
+		'lxml>=4.8.0',
+		'requests>=2.25.1',
+		'cloudscraper>=1.2.58',
+		'js2py>=0.71',
+		'websockets>=10.1',
+		'regex>=2022.3.15'
+	],
 	packages=['python_aternos'],
 	python_requires=">=3.7",
 )
