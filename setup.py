70fd2239e0d0245765b37800bcffb7b4ff3d6bbf
DarkCat09 <a.chechkenev@yandex.ru>
2022-10-05 14:36:16 +0400

    Changed links in setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/DarkCat09/python-aternos',
     project_urls={
-        'Homepage': 'https://codeberg.org/DarkCat09/python-aternos',
+        'Documentation': 'https://python-aternos.codeberg.page',
         'GitHub': 'https://github.com/DarkCat09/python-aternos',
         'Bug Tracker': 'https://github.com/DarkCat09/python-aternos/issues',
-        'Bug Tracker 2': 'https://codeberg.org/DarkCat09/python-aternos/issues'
     },
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -32,14 +31,14 @@ setuptools.setup(
         'Operating System :: MacOS',
         'Intended Audience :: Developers',
         'Topic :: Internet',
-        'Typing :: Typed'
+        'Typing :: Typed',
     ],
     install_requires=[
         'lxml>=4.8.0',
         'cloudscraper>=1.2.60',
         'js2py>=0.71',
         'websockets>=10.1',
-        'regex>=2022.3.15'
+        'regex>=2022.3.15',
     ],
     packages=['python_aternos'],
     python_requires=">=3.7",
