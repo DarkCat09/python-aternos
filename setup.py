3509504f4e0db4b617360532cb2af52ccf95e98a
DarkCat09 <a.chechkenev@yandex.ru>
2023-07-04 14:47:45 +0400

    v3.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'rt') as readme:
 
 setuptools.setup(
     name='python-aternos',
-    version='3.0.2',
+    version='3.0.3',
     author='Chechkenev Andrey (@DarkCat09)',
     author_email='aacd0709@mail.ru',
     description='An unofficial Aternos API',
